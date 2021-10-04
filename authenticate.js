9697bad35f6eacef4923630d936acaf4a809dee4
manhhungpm <hungnm.nuce60@gmail.com>
2021-10-04 22:10:06 +0700

    ass 3

--- authenticate.js
+++ authenticate.js
@@ -38,3 +38,17 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = function (req, res, next) {
+
+    // console.log(req.user.admin);
+
+    if (req.user.admin == true) {
+        next();
+    } else {
+        var err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+
+};
--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,20 @@ var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
-});
+// router.get('/', function (req, res, next) {
+//     res.send('respond with a resource');
+// });
+
+router.route("/")
+    .get(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
+        User.find({})
+            .then((promotions) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promotions);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
 
 router.post('/signup', (req, res, next) => {
     User.register(new User({username: req.body.username}),
